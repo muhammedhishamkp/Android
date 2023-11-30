0c2b9712b9597db29b7ebe00707073639a56606e
muhammedhishamkp <125430816+muhammedhishamkp@users.noreply.github.com>
2023-11-30 10:39:38 +0530

    Add files via upload

---
+++ Cycle3/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sjcet.menuapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.Menu;
+import android.widget.Toast;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.hello:
+                Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public void logout()
+    {
+        Toast.makeText(this,"bye",Toast.LENGTH_SHORT).show();
+    }
+}
